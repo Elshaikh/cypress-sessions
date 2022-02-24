ed3d4770d9bf48a346802e137dc2cd54c8614a4e
elshaikh <ahmed.sheikh@seera.sa>
2022-02-24 11:29:01 +0400

    fixes

--- cypress/integration/cypress-sessions/taskOne.js
+++ cypress/integration/cypress-sessions/taskOne.js
@@ -3,19 +3,16 @@
 /********************
  *
  * To execute tests from this spec  =>
- * Navigate to qa-frontend-cypress folder, open command line and type:
  * Yarn cypress run cypress/integration/cypress-sessions/taskOne.js --headed
- * Environment variable 'stage' can be one of this values: [dev, staging, production]; if stage is not specified tests will be run on staging
  */
 
-describe('example to-do app', () => {
+describe('1st task Cypress', () => {
   beforeEach(() => {
-
     cy.visit('http://10.72.4.107:5500/example/e.html')
   })
 
-  it('displays two todo items by default', () => {
-    cy.get('[placeholder="Street address"]').type('test')
+  it('fill the address', () => {
+    cy.get('[placeholder="Street address"]').type('al barsha 1 beside MOE')
     
     })
   })
